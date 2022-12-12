d85577d0cfaa08eea86c57a4f976374b8c141216
Serhii Onyshchenko <62890013+SerhiiOnyshchenko@users.noreply.github.com>
2022-12-12 19:10:35 +0200

    fix2

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -8,14 +8,10 @@ export default function Loader() {
   const inTop = useSelector(getLoader);
   const loaderRoot = useRef(document.querySelector('#loader'));
   return createPortal(
-		<>
-    <div
-      className={'Loader Loader1' + ' ' + (inTop ? 'Loader-in-top' : '')}
-    ></div>
-    <div
-      className={'Loader Loader2' + ' ' + (inTop ? 'Loader-in-top' : '')}
-    ></div>
-		</>,
+    <>
+      <div className={`Loader Loader1 ${inTop ? 'Loader-in-top' : ''}`}></div>
+      <div className={`Loader Loader2 ${inTop ? 'Loader-in-top' : ''}`}></div>
+    </>,
     loaderRoot.current
   );
 }
